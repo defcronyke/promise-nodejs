39552738f2ce47524ba9ac6094486cf233cf27e4
Daphne <machinegirl@eternalvoid.net>
2016-09-19 16:15:33 -0400

    Bug: Promises called at the same time in all() and race() should work, but they don't

--- test-official-promises.js
+++ test-official-promises.js
@@ -1,49 +1,72 @@
-new Promise((resolve, reject) => {
-    resolve('bud');
-    // reject('butt');
-}).then((res) => {
-    console.log('res1: ' + res);
-}).catch((err) => {
-    console.log('err1: ' + err);
-}).then((res) => {
-    console.log('res2: ' + res);
-}).catch((err) => {
-    console.log('err2: ' + err);
+// new Promise((resolve, reject) => {
+//     resolve('bud');
+//     // reject('butt');
+// }).then((res) => {
+//     console.log('res1: ' + res);
+// }).catch((err) => {
+//     console.log('err1: ' + err);
+// }).then((res) => {
+//     console.log('res2: ' + res);
+// }).catch((err) => {
+//     console.log('err2: ' + err);
+// });
+
+var p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('cats');
+    }, 1000);
 });
 
-Promise.all([
-    new Promise((resolve, reject) => {
-        resolve('duck');
-    }),
-    // new Promise((resolve, reject) => {
-    //     setTimeout(() => {
-    //         resolve('pant');
-    //     }, 1000 * 1);
-    // }),
-    // Promise.reject('no dogs!'),
-    Promise.resolve('dogs')
+var p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('rats');
+    }, 1000 * 2);
+});
 
-]).then((res) => {
+var p3 = Promise.resolve('bats');
+
+
+Promise.all([p1, p2, p3]).then((res) => {
     console.log('Promise.all res: ' + JSON.stringify(res));
-}).catch((err) => {
-    console.log('Promise.all err: ' + JSON.stringify(err))
 });
 
-Promise.race([
-    // Promise.reject('no dogs!'),
-    new Promise((resolve, reject) => {
-        resolve('duck');
-    }),
-    // new Promise((resolve, reject) => {
-    //     setTimeout(() => {
-    //         resolve('pant');
-    //     }, 1000 * 1);
-    // }),
-    Promise.reject('no dogs!'),
-    Promise.resolve('dogs')
+Promise.race([p1, p2, p3]).then((res) => {
+    console.log('Promise.race res: ' + res);
+})
 
-]).then((res) => {
-    console.log('Promise.race res: ' + JSON.stringify(res));
-}).catch((err) => {
-    console.log('Promise.race err: ' + JSON.stringify(err))
-});
+// Promise.all([
+//     new Promise((resolve, reject) => {
+//         resolve('duck');
+//     }),
+//     // new Promise((resolve, reject) => {
+//     //     setTimeout(() => {
+//     //         resolve('pant');
+//     //     }, 1000 * 1);
+//     // }),
+//     // Promise.reject('no dogs!'),
+//     Promise.resolve('dogs')
+//
+// ]).then((res) => {
+//     console.log('Promise.all res: ' + JSON.stringify(res));
+// }).catch((err) => {
+//     console.log('Promise.all err: ' + JSON.stringify(err))
+// });
+//
+// Promise.race([
+//     // Promise.reject('no dogs!'),
+//     new Promise((resolve, reject) => {
+//         resolve('duck');
+//     }),
+//     // new Promise((resolve, reject) => {
+//     //     setTimeout(() => {
+//     //         resolve('pant');
+//     //     }, 1000 * 1);
+//     // }),
+//     Promise.reject('no dogs!'),
+//     Promise.resolve('dogs')
+//
+// ]).then((res) => {
+//     console.log('Promise.race res: ' + JSON.stringify(res));
+// }).catch((err) => {
+//     console.log('Promise.race err: ' + JSON.stringify(err))
+// });
--- test.js
+++ test.js
@@ -1,51 +1,75 @@
 var Promise = require('./promise.js');
 
-new Promise((resolve, reject) => {
-    resolve('bud');
-    // reject('butt');
-}).then((res) => {
-    console.log('res1: ' + res);
-}).catch((err) => {
-    console.log('err1: ' + err);
-}).then((res) => {
-    console.log('res2: ' + res);
-}).catch((err) => {
-    console.log('err2: ' + err);
+// new Promise((resolve, reject) => {
+//     resolve('bud');
+//     // reject('butt');
+// }).then((res) => {
+//     console.log('res1: ' + res);
+// }).catch((err) => {
+//     console.log('err1: ' + err);
+// }).then((res) => {
+//     console.log('res2: ' + res);
+// }).catch((err) => {
+//     console.log('err2: ' + err);
+// });
+//
+// Promise.all([
+//     new Promise((resolve, reject) => {
+//         resolve('duck');
+//     }),
+//     // new Promise((resolve, reject) => {
+//     //     setTimeout(() => {
+//     //         resolve('pant');
+//     //     }, 1000 * 1);
+//     // }),
+//     // Promise.reject('no dogs!'),
+//     Promise.resolve('dogs')
+//
+// ]).then((res) => {
+//     console.log('Promise.all res: ' + JSON.stringify(res));
+// }).catch((err) => {
+//     console.log('Promise.all err: ' + JSON.stringify(err))
+// });
+//
+// Promise.race([
+//     // Promise.reject('no dogs!'),
+//     new Promise((resolve, reject) => {
+//         resolve('duck');
+//     }),
+//     // new Promise((resolve, reject) => {
+//     //     setTimeout(() => {
+//     //         resolve('pant');
+//     //     }, 1000 * 1);
+//     // }),
+//     Promise.reject('no dogs!'),
+//     Promise.resolve('dogs')
+//
+// ]).then((res) => {
+//     console.log('Promise.race res: ' + JSON.stringify(res));
+// }).catch((err) => {
+//     console.log('Promise.race err: ' + JSON.stringify(err))
+// });
+
+
+var p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('cats');
+    }, 1000);
 });
 
-Promise.all([
-    new Promise((resolve, reject) => {
-        resolve('duck');
-    }),
-    // new Promise((resolve, reject) => {
-    //     setTimeout(() => {
-    //         resolve('pant');
-    //     }, 1000 * 1);
-    // }),
-    // Promise.reject('no dogs!'),
-    Promise.resolve('dogs')
+var p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('rats');
+    }, 1000 * 2);
+});
 
-]).then((res) => {
+var p3 = Promise.resolve('bats');
+
+
+Promise.all([p1, p2, p3]).then((res) => {
     console.log('Promise.all res: ' + JSON.stringify(res));
-}).catch((err) => {
-    console.log('Promise.all err: ' + JSON.stringify(err))
 });
 
-Promise.race([
-    // Promise.reject('no dogs!'),
-    new Promise((resolve, reject) => {
-        resolve('duck');
-    }),
-    // new Promise((resolve, reject) => {
-    //     setTimeout(() => {
-    //         resolve('pant');
-    //     }, 1000 * 1);
-    // }),
-    Promise.reject('no dogs!'),
-    Promise.resolve('dogs')
-
-]).then((res) => {
-    console.log('Promise.race res: ' + JSON.stringify(res));
-}).catch((err) => {
-    console.log('Promise.race err: ' + JSON.stringify(err))
+Promise.race([p1, p2, p3]).then((res) => {
+    console.log('Promise.race res: ' + res);
 });
